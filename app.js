92e7e017817c3818e23aaf7858010fee26841926
randychilau <90356410+randychilau@users.noreply.github.com>
2022-07-28 15:52:22 -0700

    correcting eslint errors

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const app = express()
 
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   console.log('server started on port 5000')
-});
+})
