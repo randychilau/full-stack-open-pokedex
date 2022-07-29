614a3a7439b380d2992852d83058b4d80b77283f
randychilau <90356410+randychilau@users.noreply.github.com>
2022-07-28 17:05:45 -0700

    broken up into two tests

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -3,7 +3,10 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+  it('able to access pokemon page', function() {
     cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
   })
+  
 })
